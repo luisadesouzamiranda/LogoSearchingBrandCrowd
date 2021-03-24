7857c4e6e11a2c9e6dab907e5268aeac0c4ba440
Luisa De Souza Miranda <luisadesouzamiranda@gmail.com>
2021-03-24 15:45:26 +1100

    Create test case and destructuring object

--- cypress/integration/logo_search.js
+++ cypress/integration/logo_search.js
@@ -8,19 +8,35 @@ describe("LogoSearch", () => {
     });
   });
   it("Search should return result with no searching criteria", function () {
-    SearchLogo.navigateTo(this.testdata.url);
-    Page.elementContainsText(this.testdata.logoPage);
+    const { url, logoPage } = this.testdata;
+    SearchLogo.navigateTo(url);
+    Page.elementContainsText(logoPage);
     SearchLogo.clickButton('*[class="button alert"]', "create");
     SearchLogo.checkIfSearchHasResult(".responsive-embed");
   });
 
   it("Search should return result with Business name searching criteria", function () {
-    SearchLogo.inputBusinessName("#gtm-SearchText", this.testdata.businessName);
+    const { businessName } = this.testdata;
+    SearchLogo.inputBusinessName("#gtm-SearchText", businessName);
     SearchLogo.clickButton('*[class="button alert"]', "create");
     SearchLogo.checkIfResultRelevant(
       ".responsive-embed__image",
-      `text=${this.testdata.businessName}`
+      `text=${businessName}`
     );
     SearchLogo.clickBackonBrowser();
+  });
+  it("Search should return result with all searching criterias", function () {
+    const { businessName, keywords } = this.testdata;
+    SearchLogo.clickButton("[id^=filter-bar]", "More");
+    SearchLogo.inputBusinessName("#gtm-SearchText", businessName);
+    SearchLogo.inputKeywords("#SearchText", keywords);
+    SearchLogo.selectDropDown("#LogoStyle", "Icon & Text", "1");
+    SearchLogo.checkDropdown("#font-styles-dropdown", "Vintage");
+    SearchLogo.checkDropdown("#colors-dropdown", "Grayscale");
+    SearchLogo.clickButton('*[class="button alert"]', "create");
+    SearchLogo.checkIfResultRelevant(
+      ".responsive-embed__image",
+      `text=${businessName}`
+    );
   });
 });
